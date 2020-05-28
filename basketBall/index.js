95921e6fca402a37f935049bec94d97e517d93bd
nishchay17 <nishchay13971@gmail.com>
2020-05-28 20:25:52 +0530

    fix

--- basketBall/index.js
+++ basketBall/index.js
@@ -1,3 +1,4 @@
 addEventListener("load", () => {
-  if (window.location.hash != "") window.location = "/";
+  if (window.location.hash != "")
+    window.location = "https://nishchay17.github.io/basketBall/main.html";
 });
